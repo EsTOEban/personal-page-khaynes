e4e9bb841ec0daf7ce63e2b0083d6061a26df5c9
Kevin Haynes <kevin.haynes.dev@gmail.com>
2020-11-15 15:43:55 -0500

    Establish top bar and setup for additional pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule} from './top-bar/material-module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import { ResumeComponent } from './resume/resume.component';
+import { GiftIdeasComponent } from './gift-ideas/gift-ideas.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    // RouterModule.forRoot([
+    //   {path: '', component: MainPageComponent},
+    //   {path: '/resume', component: ResumeComponent},
+    //   {path: '/gift-ideas', component: GiftIdeasComponent}
+    // ]),
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ResumeComponent,
+    GiftIdeasComponent,
+    MainPageComponent
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ]
 })
 export class AppModule { }
