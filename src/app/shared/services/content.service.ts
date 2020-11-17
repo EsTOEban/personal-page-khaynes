3cefdefcfd9d1ff7e26dec3c9a2f877328b86940
Kevin Haynes <kevin.haynes.dev@gmail.com>
2020-11-16 20:41:18 -0500

    Testing Deployed Changes being made: CD finally worked

--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -8,7 +8,7 @@ export class ContentService {
   constructor() { }
 
   pages = {
-    home: {title: 'Home', subtitle: 'Welcome Home! - subtitle', content: 'Some home content.', image: 'assets/bg00.jpg'},
+    home: {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', image: 'assets/bg00.jpg'},
     about: {title: 'Resume', subtitle: 'Resume', content: 'Some resume content .', image: 'assets/bg01.jpg'},
     brianna: {title: 'Brianna\'s Gift Ideas', subtitle: 'Contact Us', content: 'How to contact us.', image: 'assets/bg02.jpg'}
   };
